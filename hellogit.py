7b8a61df0f16c7338d32b37fbaf8f2000549d8f8
jvmalave <malavemjv@gmail.com>
2026-04-15 22:10:37 -0400

    Cambio 1123

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,3 @@
 print("New Hello Git!  This is my first Git repository.")
-print("I am learning how to use Git for version control.")
+
 print("This is a simple Python script to demonstrate Git usage.") 
